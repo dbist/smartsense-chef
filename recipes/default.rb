736309d4be50ca49faea60bef51d1d7af02f7018
Artem Ervits <generic13@gmail.com>
2016-05-20 15:56:58 -0400

    successfully install the smartsense tool

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# copy smartsense package to /tmp directory
 cookbook_file "/tmp/smartsense-hst_1.2.1-161_amd64.deb" do
    source "smartsense-hst_1.2.1-161_amd64.deb"
    mode "0770"	
 end
 
+# install smartsense package on a node
 dpkg_package "/tmp/smartsense-hst_1.2.1-161_amd64.deb" do
-#  source "smartsense-hst_1.2.1-161_amd64.deb"
   action :install	
 end
 
