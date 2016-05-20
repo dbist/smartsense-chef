43453e3ff7242ec1019b5477546a5e8afc982938
Artem Ervits <generic13@gmail.com>
2016-05-20 15:39:57 -0400

    moving file to /tmp works

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+cookbook_file "/tmp/smartsense-hst_1.2.1-161_amd64.deb" do
+   source "smartsense-hst_1.2.1-161_amd64.deb"
+   mode "0770"	
+end
+
+# dpkg_package "/tmp/smartsense-hst_1.2.1-161_amd64.deb" do
+#   source "smartsense-hst_1.2.1-161_amd64.deb"
+#   action :install	
+# end
+
+
