739021723daa63cabebd580029c8a9b4b9690961
Artem Ervits <generic13@gmail.com>
2016-05-30 21:47:24 -0400

    fixed FC064

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description 'Installs/Configures smartsense-chef'
 long_description 'Installs/Configures smartsense-chef'
 version '0.1.0'
 source_url 'https://github.com/dbist/smartsense-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/dbist/smartsense-chef' if respond_to?(:issues_url)
 
 depends 'java'
