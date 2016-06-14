b1d725608475c37b36bd78cbafa6c9e62595af03
Artem Ervits <generic13@gmail.com>
2016-06-14 14:51:02 -0400

    added global variable for better readability

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,25 +5,26 @@
 # Copyright (c) 2016 Artem Ervits, All Rights Reserved.
 
 include_recipe "smartsense-chef::smartsense_setup"
-
-remote_file "#{Chef::Config[:file_cache_path]}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb" do
-   source "#{node['smartsense-chef']['repo_url']}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb"
+filename = "#{Chef::Config[:file_cache_path]}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb"
+package_src = "#{node['smartsense-chef']['repo_url']}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb"
+remote_file filename do
+   source package_src
    mode '0770'
    action :create_if_missing
    checksum node['smartsense-chef']['checksum']
-   not_if { node['smartsense-chef']['use_local_repo'] }
+   not_if { node['smartsense-chef']['use_local_repo'] || File::exists?(filename) }
 end
 
 # install smartsense package on a node
 dpkg_package "#{Chef::Config[:file_cache_path]}/smartsense-hst_1.2.2-0_amd64.deb" do
-  action :install	
+   action :install
+   not_if { node['smartsense-chef']['use_local_repo'] }	
 end
 
-# package "smartsense-hst" do
-#    action :install
-#    version '1.2.2-0_amd64'
-#    provider Chef::Provider::Package::Apt
-# end
+package "smartsense-hst" do
+   only_if { node['smartsense-chef']['use_local_repo'] }
+#   version '1.2.2'
+end
 
 # SmartSense results are more accurate with the following dependencies installed
 %w{wget sysstat dstat lsof net-tools python2.7}.each do |pkg|
