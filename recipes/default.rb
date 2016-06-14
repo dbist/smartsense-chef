7d285e4ca01348a8172259ae6d1680184ac231a7
Artem Ervits <generic13@gmail.com>
2016-06-14 13:12:14 -0400

    removed extraneous comment

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,9 +6,7 @@
 
 include_recipe "smartsense-chef::smartsense_setup"
 
-remote_file "#{Chef::Config[:file_cache_path]}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb" do   
-#   source 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.2.2.0/pool/main/s/smartsense-hst/smartsense-hst_1.2.2-0_amd64.deb'
-   source "#{node['smartsense-chef']['repo_url']}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb"
+remote_file "#{Chef::Config[:file_cache_path]}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb" do    source "#{node['smartsense-chef']['repo_url']}/smartsense-hst_#{node['smartsense-chef']['smartsense_version']}.deb"
    mode '0770'
    action :create_if_missing
    checksum node['smartsense-chef']['checksum']
